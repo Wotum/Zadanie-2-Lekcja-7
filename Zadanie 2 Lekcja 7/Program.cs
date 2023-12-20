361002c4d73fb28168dc808a7e69da3e90484c5e
HARDPC <HARDPC@WOTUM>
2023-12-20 18:23:31 +0100

    Add project files.

---
+++ Zadanie 2 Lekcja 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KatoApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Nadanie zmiennej int wartości
+            int liczba = 15;
+
+
+            //Porównanie zmiennych przy użyciu operatora 
+            if (liczba % 2 == 0)
+            {
+                Console.WriteLine(liczba + " jest liczbą parzystą"); //Wyświetlenie komunikatu, gdy liczba jest parzysta
+            }
+            else
+            {
+                Console.WriteLine(liczba + " jest liczbą nieparzystą"); //Wyświetlenie komunikatu, gdy liczba jest nieparzysta
+            }
+        }
+
+    }
+
+
+}
